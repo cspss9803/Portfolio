d8967ddb53e15a30b0a3e79029aff46d192f82ff
cspss9803 <90231876+cspss9803@users.noreply.github.com>
2022-05-19 17:40:45 +0800

    Add files via upload

---
+++ AGP/js/loading.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var n = 0
+    var interval = setInterval(function(){
+            if(n<100){
+                n++;
+                $('#number').text(n+"%");
+                if(n>=100){
+                    clearInterval(interval)
+                }
+            }
+        },50)
+    setTimeout(function(){
+        $('#m,#number').fadeOut(400);
+        $('#loading h1').delay(300).fadeIn(400);
+        $('#loading').delay(1900).fadeOut(400);
+    },5000)
+});
